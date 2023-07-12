7f173fff7534b8d7a5aaf886bbceb338dd952131
lazy-shoebill <groovin_high@i-iide.com>
2023-07-12 13:08:49 +0900

    add process for excluding labor cost

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import calendar
-# import seaborn as sns
 import datetime
-
 import pandas as pd
 import streamlit as st
 
 
-def get_file_name(file):
+def get_year_month_from_file(file):
     # ファイル名から年月文字列を取得
     file_name = file.name.split('.')[0].split('_')[-1]
     return file_name
 
 
-def get_eom(str_date):
+def get_end_of_month_date(str_yyyymm):
     # ファイル名から月末日付を取得
-
-    year = int(str_date[:4])
-    month = int(str_date[-2:])
+    year = int(str_yyyymm[:4])
+    month = int(str_yyyymm[-2:])
     last_day = calendar.monthrange(year, month)[1]
 
     eom = datetime.date(year, month, last_day).strftime('%Y/%m/%d')
     return eom
+
+
+def get_df_info(df):
+    # データフレームからファイル容量、サイズ、欠損値の有無を取得
+    data_shape = df.shape
+    data_size = df.memory_usage().sum()
+    count_null = df.isnull().any().sum()
+
+    return data_shape, data_size, count_null
 
 
 @st.cache
@@ -32,35 +38,50 @@ def load_file(file):
 # --- 仕訳データの変換処理 ----
 def filtered_df(df):
     # 並び替えとカラムの整理、リネーム
+
+    # カラム名変更用辞書（一次処理）
+    dict_account_conversion = {
+        '借方科目コード': 'dr_cd',
+        '借方科目名称': 'dr_name',
+        '借方科目別補助コード': 'dr_sub_cd',
+        '借方科目別補助名称': 'dr_sub_name',
+        '借方部門コード': 'dr_section_cd',
+        '借方部門名称': 'dr_section_name',
+        '借方セグメント2': 'dr_segment_cd',
+        '借方セグメント２名称': 'dr_segment_name',
+        '貸方科目コード': 'cr_cd',
+        '貸方科目名称': 'cr_name',
+        '貸方科目別補助コード': 'cr_sub_cd',
+        '貸方科目別補助名称': 'cr_sub_name',
+        '貸方部門コード': 'cr_section_cd',
+        '貸方部門名称': 'cr_section_name',
+        '貸プセグメント2コード': 'cr_segment_cd',
+        '貸方セグメント２名称': 'cr_segment_name',
+        '金額': 'price',
+        '消費税': 'tax',
+        '摘要': 'outline'
+    }
+
     df = df.filter(
-        ['借方科目コード', '借方科目名称', '借方科目別補助コード', '借方科目別補助名称', '借方部門コード', '借方部門名称',
-         '借方セグメント2', '借方セグメント２名称', '貸方科目コード', '貸方科目名称', '貸方科目別補助コード', '貸方科目別補助名称',
-         '貸方部門コード', '貸方部門名称', '貸プセグメント2コード', '貸方セグメント２名称', '金額', '消費税', '摘要']) \
-        .set_axis(['dr_cd', 'dr_name', 'dr_sub_cd', 'dr_sub_name', 'dr_section_cd', 'dr_section_name', 'dr_segment_cd',
-                   'dr_segment_name',
-                   'cr_cd', 'cr_name', 'cr_sub_cd', 'cr_sub_name', 'cr_section_cd', 'cr_section_name', 'cr_segment_cd',
-                   'cr_segment_name',
-                   'price', 'tax', 'outline'], axis=1
-                  )
+            ['借方科目コード', '借方科目名称', '借方科目別補助コード', '借方科目別補助名称', '借方部門コード', '借方部門名称',
+             '借方セグメント2', '借方セグメント２名称', '貸方科目コード', '貸方科目名称', '貸方科目別補助コード', '貸方科目別補助名称',
+             '貸方部門コード', '貸方部門名称', '貸プセグメント2コード', '貸方セグメント２名称', '金額', '消費税', '摘要'])\
+        .rename(dict_account_conversion, axis=1)
+
     return df
 
 
 def convert_df(file):
-    # 元ファイルの読み込み
+    # データの読み込み
     df = load_file(file)
-    # コードの変換とフィルター
+    # 必要なカラムのフィルタリングとコード変換
     filtered = filtered_df(df)
     return filtered
 
 
-def copy_dataframe(df):
-    _df = df.copy().assign(price=lambda x: df['price'] - df['tax']).drop('tax', axis=1)
-    return _df
-
-
-# -- 借方データ --
+# -- 借方データの整形処理 --
 def convert_dr(df):
-    df = copy_dataframe(df)
+    df = df.copy().assign(price=lambda x: df['price'] - df['tax']).drop('tax', axis=1)
 
     # 貸方項目を削除
     _df = df.drop(
@@ -89,7 +110,7 @@ def calc_dr(df):
 
 # -- 貸方データ --
 def convert_cr(df):
-    df = copy_dataframe(df)
+    df = df.copy().assign(price=lambda x: df['price'] - df['tax']).drop('tax', axis=1)
 
     # 貸方項目を削除
     _df = df.drop(
@@ -117,7 +138,7 @@ def calc_cr(df):
 
 
 # -- データ統合 --
-def concat_drcr(dr, cr):
+def concat_df(dr, cr):
     df = pd.concat([dr, cr]).reset_index(drop=True)
     df.dropna(subset='ac_cd', inplace=True)
     # 型変換
@@ -192,22 +213,23 @@ def load_long_data(df):
     df = load_file(df)
     df = melt_df(df)
     df = add_mapping(df)
-    df= df.fillna(0)
+    df = df.fillna(0)
     df.dropna(subset='金額', inplace=True)
     df['科目CD'] = df['科目CD'].apply(lambda x: str(int(x)))
     df['補助科目CD'] = df['補助科目CD'].apply(lambda x: str(int(x)))
     df['部門CD'] = df['部門CD'].apply(lambda x: str(int(x)))
-    # df['金額'] = df['金額'].apply(lambda x: float(x))
+
     return df
+
 
 @st.cache
 def convert_to_csv(df, index=False):
-    return df.to_csv().encode('cp932')
+    return df.to_csv(index=False).encode('cp932')
 
 
 def main():
     """
-    Streamlit Application
+    Streamlit Main Application
     :return:
     """
 
@@ -219,12 +241,14 @@ def main():
     side_header = st.sidebar.container()
     side_header.write('--FILE UPLOADER--')
     side_header.write('#### 1.振替伝票 → 単票形式')
+    side_header.caption('複式簿記形式 → 単票形式への組み換え')
 
     uploaded_file = side_header.file_uploader('ファイル名：◯◯◯◯_YYYYMM', type='csv')
 
     side_header.write('---')
 
     side_header.write('#### 2.配賦データ → 縦変換')
+    side_header.caption('各セグメントへ配賦した横データ(wide)を縦型(long)に組み換え')
 
     flg_box = side_header.radio('予算/実績の区分を選択', ('実績', '予算'))
     uploaded_wide_file = side_header.file_uploader('ファイル名: ◯◯◯◯_YYYYMM', type='csv')
@@ -235,38 +259,69 @@ def main():
 
     # １次処理用
     if uploaded_file is not None:
-        # get_ym = get_file_name(uploaded_file)
-
-        # ファイルの読み込み
+        # データの読み込み
         df = convert_df(uploaded_file)
 
-        # 借方データの変換
+        # 借方データの整形処理
         df_dr = calc_dr(df)
 
-        # 貸方データの変換
+        # 貸方データの整形処理
         df_cr = calc_cr(df)
 
-        # 短表データの連結
-        df_concat = concat_drcr(df_dr, df_cr)
+        # 貸借データの縦連結
+        df_concat = concat_df(df_dr, df_cr)
+        concat_data_shape, concat_data_size, concat_count_null = get_df_info(df_concat)
+
+        # 人件費に関するコードリスト
+        labor_cost_cd_list = [
+            '6110', '6120', '6130', '6140', '6150', '6160', '6170', '6180', '6190', '6200',
+            '7110', '7120', '7130', '7140', '7150', '7160', '7170', '7180', '7190', '7200'
+        ]
+
+        # ac_cdからlabor_cost_cd_listにないものだけをリスト化
+        value_list = list(set(df_concat['ac_cd'].values))
+        target_list = sorted([item for item in value_list if item not in labor_cost_cd_list])
+
+        # 人件費項目を除外したデータフレームを作成
+        df_exclude_labor_cost = df_concat.query('ac_cd in @target_list')
+        exc_data_shape, exc_data_size, exc_count_null = get_df_info(df_exclude_labor_cost)
 
         container_main.subheader('1-1. Result - Details')
-        data_size = df_concat.memory_usage().sum()
 
         show_detail = container_main.checkbox('Check & Preview - Details!')
 
         if show_detail:
             container_main.dataframe(df_concat, use_container_width=True)
 
-        output_result_detail = convert_to_csv(df_concat)
+        output_result_detail = convert_to_csv(df_concat, index=False)
 
         container_main.download_button(
             label='DL: 詳細データ',
             data=output_result_detail,
-            file_name=f'result_detail_{get_file_name(uploaded_file)}.csv',
+            file_name=f'result_detail_{get_year_month_from_file(uploaded_file)}.csv',
             mime='text/csv'
         )
         container_main.caption(
-            f'参考）サイズ: {df_concat.shape},　容量: {data_size / 1024:.1f} MB, データ欠損値: {df_concat.isnull().any().sum()}')
+            f'参考）サイズ: {concat_data_shape},　容量: {concat_data_size / 1024:.1f} MB, データ欠損値: {concat_count_null}')
+
+        container_main.write('---')
+        # 人件費科目を除外したデータの処理
+        output_result_exclude_labor_cost = convert_to_csv(df_exclude_labor_cost, index=False)
+
+        show_detail_exclude = container_main.checkbox('Check & Preview - Exclude Labor Cost')
+
+        if show_detail_exclude:
+            container_main.dataframe(df_exclude_labor_cost, use_container_width=True)
+
+        container_main.download_button(
+            label='DL: 詳細(人件費除き)',
+            data=output_result_exclude_labor_cost,
+            file_name=f'result_exclude_labor_cost_{get_year_month_from_file(uploaded_file)}.csv',
+            mime='text/csv'
+        )
+        container_main.caption(
+            f'参考）サイズ: {exc_data_shape},　容量: {exc_data_size / 1024:.1f} MB, データ欠損値: {exc_count_null}')
+
 
         container_main.write('---')
         container_main.subheader('1-2. Result - Grouped')
@@ -288,7 +343,7 @@ def main():
         container_main.download_button(
             label='DL: 集計データ',
             data=output_result_grouped,
-            file_name=f'result_{get_file_name(uploaded_file)}.csv',
+            file_name=f'result_{get_year_month_from_file(uploaded_file)}.csv',
             mime='text/csv'
         )
 
@@ -308,15 +363,15 @@ def main():
 
         if flg_box == '実績':
             df['予算/実績'] = '実績'
-            df['期間'] = get_eom(get_file_name(uploaded_wide_file))
+            df['期間'] = get_end_of_month_date(get_year_month_from_file(uploaded_wide_file))
 
         elif flg_box == '予算':
             df['予算/実績'] = '予算'
-            df['期間'] = get_eom(get_file_name(uploaded_wide_file))
+            df['期間'] = get_end_of_month_date(get_year_month_from_file(uploaded_wide_file))
 
         else:
             df['予算/実績'] = ''
-            df['期間'] = get_eom(get_file_name(uploaded_wide_file))
+            df['期間'] = get_end_of_month_date(get_year_month_from_file(uploaded_wide_file))
 
         df_result_long = df.filter(
             ['予算/実績', '期間', '科目CD', '科目名', '補助科目CD', '補助科目名', '部門CD', '部門名', '集計区分', 's_class', 'mid_class',
@@ -343,7 +398,7 @@ def main():
         container_main.download_button(
             label='DL: 売上データ（long型）',
             data=output_result_sales_long,
-            file_name=f'result_sales_long_{get_file_name(uploaded_wide_file)}.csv',
+            file_name=f'result_sales_long_{get_year_month_from_file(uploaded_wide_file)}.csv',
             mime='text/csv'
         )
         container_main.caption(
@@ -364,7 +419,7 @@ def main():
         container_main.download_button(
             label='DL: 経費データ（long型）',
             data=output_result_cost_long,
-            file_name=f'result_cost_long_{get_file_name(uploaded_wide_file)}.csv',
+            file_name=f'result_cost_long_{get_year_month_from_file(uploaded_wide_file)}.csv',
             mime='text/csv'
         )
         container_main.caption(
